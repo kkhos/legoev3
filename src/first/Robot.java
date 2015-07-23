978363326beefa1ca63001e9c4984de836d6d04d
hawk-eye <kkhos123@gmail.com>
2015-07-24 01:05:29 +0900

    git init

---
+++ src/first/Robot.java
@@ -0,0 +1,14 @@
+package first;
+
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+public class Robot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LCD.drawString("Plugin Test", 0, 4);
+		Delay.msDelay(5000);
+	}
+
+}
